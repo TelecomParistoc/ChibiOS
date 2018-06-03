4cec040a1f7c61b757e8c4384adc6ded8cd506fc
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.org>
2018-06-03 15:46:35 +0200

    Initial commit

---
+++ demos/STM32/RT-STM32F302R8-NUCLEO/.dep/chcore.o.d
@@ -0,0 +1,128 @@
+build/obj/chcore.o: ../../../os/rt/ports/ARMCMx/chcore.c \
+ ../../../os/rt/include/ch.h \
+ ../../../os/rt/ports/ARMCMx/compilers/GCC/chtypes.h \
+ /usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stddef.h \
+ /usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h \
+ /usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/../../../../arm-none-eabi/include/stdint.h \
+ /usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdbool.h chconf.h \
+ ../../../os/rt/include/chlicense.h ../../../os/rt/include/chsystypes.h \
+ ../../../os/rt/ports/ARMCMx/chcore.h \
+ ../../../os/common/ports/ARMCMx/devices/STM32F3xx/cmparams.h \
+ ../../../os/hal/boards/ST_NUCLEO_F302R8/board.h \
+ ../../../os/ext/CMSIS/ST/STM32F3xx/stm32f3xx.h \
+ ../../../os/ext/CMSIS/ST/STM32F3xx/stm32f302x8.h \
+ ../../../os/ext/CMSIS/include/core_cm4.h \
+ ../../../os/ext/CMSIS/include/core_cmInstr.h \
+ ../../../os/ext/CMSIS/include/core_cmFunc.h \
+ ../../../os/ext/CMSIS/include/core_cmSimd.h \
+ ../../../os/ext/CMSIS/ST/STM32F3xx/system_stm32f3xx.h \
+ ../../../os/rt/ports/ARMCMx/chcore_v7m.h \
+ ../../../os/rt/ports/ARMCMx/chcore_timer.h ../../../os/hal/include/st.h \
+ ../../../os/hal/ports/STM32/LLD/TIMv1/st_lld.h mcuconf.h \
+ ../../../os/hal/ports/STM32/STM32F3xx/stm32_registry.h \
+ ../../../os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h \
+ ../../../os/rt/include/chdebug.h ../../../os/rt/include/chtm.h \
+ ../../../os/rt/include/chstats.h ../../../os/rt/include/chschd.h \
+ ../../../os/rt/include/chsys.h ../../../os/rt/include/chvt.h \
+ ../../../os/rt/include/chthreads.h ../../../os/rt/include/chregistry.h \
+ ../../../os/rt/include/chsem.h ../../../os/rt/include/chbsem.h \
+ ../../../os/rt/include/chmtx.h ../../../os/rt/include/chcond.h \
+ ../../../os/rt/include/chevents.h ../../../os/rt/include/chmsg.h \
+ ../../../os/rt/include/chmboxes.h ../../../os/rt/include/chmemcore.h \
+ ../../../os/rt/include/chheap.h ../../../os/rt/include/chmempools.h \
+ ../../../os/rt/include/chdynamic.h ../../../os/rt/include/chqueues.h \
+ ../../../os/rt/include/chstreams.h
+
+../../../os/rt/include/ch.h:
+
+../../../os/rt/ports/ARMCMx/compilers/GCC/chtypes.h:
+
+/usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stddef.h:
+
+/usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h:
+
+/usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/../../../../arm-none-eabi/include/stdint.h:
+
+/usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdbool.h:
+
+chconf.h:
+
+../../../os/rt/include/chlicense.h:
+
+../../../os/rt/include/chsystypes.h:
+
+../../../os/rt/ports/ARMCMx/chcore.h:
+
+../../../os/common/ports/ARMCMx/devices/STM32F3xx/cmparams.h:
+
+../../../os/hal/boards/ST_NUCLEO_F302R8/board.h:
+
+../../../os/ext/CMSIS/ST/STM32F3xx/stm32f3xx.h:
+
+../../../os/ext/CMSIS/ST/STM32F3xx/stm32f302x8.h:
+
+../../../os/ext/CMSIS/include/core_cm4.h:
+
+../../../os/ext/CMSIS/include/core_cmInstr.h:
+
+../../../os/ext/CMSIS/include/core_cmFunc.h:
+
+../../../os/ext/CMSIS/include/core_cmSimd.h:
+
+../../../os/ext/CMSIS/ST/STM32F3xx/system_stm32f3xx.h:
+
+../../../os/rt/ports/ARMCMx/chcore_v7m.h:
+
+../../../os/rt/ports/ARMCMx/chcore_timer.h:
+
+../../../os/hal/include/st.h:
+
+../../../os/hal/ports/STM32/LLD/TIMv1/st_lld.h:
+
+mcuconf.h:
+
+../../../os/hal/ports/STM32/STM32F3xx/stm32_registry.h:
+
+../../../os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h:
+
+../../../os/rt/include/chdebug.h:
+
+../../../os/rt/include/chtm.h:
+
+../../../os/rt/include/chstats.h:
+
+../../../os/rt/include/chschd.h:
+
+../../../os/rt/include/chsys.h:
+
+../../../os/rt/include/chvt.h:
+
+../../../os/rt/include/chthreads.h:
+
+../../../os/rt/include/chregistry.h:
+
+../../../os/rt/include/chsem.h:
+
+../../../os/rt/include/chbsem.h:
+
+../../../os/rt/include/chmtx.h:
+
+../../../os/rt/include/chcond.h:
+
+../../../os/rt/include/chevents.h:
+
+../../../os/rt/include/chmsg.h:
+
+../../../os/rt/include/chmboxes.h:
+
+../../../os/rt/include/chmemcore.h:
+
+../../../os/rt/include/chheap.h:
+
+../../../os/rt/include/chmempools.h:
+
+../../../os/rt/include/chdynamic.h:
+
+../../../os/rt/include/chqueues.h:
+
+../../../os/rt/include/chstreams.h:
---
+++ demos/STM32/RT-STM32F302R8-NUCLEO/.dep/vectors.o.d
@@ -0,0 +1,41 @@
+build/obj/vectors.o: \
+ ../../../os/common/ports/ARMCMx/compilers/GCC/vectors.c \
+ /usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdbool.h \
+ /usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h \
+ /usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/../../../../arm-none-eabi/include/stdint.h \
+ ../../../os/common/ports/ARMCMx/compilers/GCC/vectors.h \
+ ../../../os/common/ports/ARMCMx/devices/STM32F3xx/cmparams.h \
+ ../../../os/hal/boards/ST_NUCLEO_F302R8/board.h \
+ ../../../os/ext/CMSIS/ST/STM32F3xx/stm32f3xx.h \
+ ../../../os/ext/CMSIS/ST/STM32F3xx/stm32f302x8.h \
+ ../../../os/ext/CMSIS/include/core_cm4.h \
+ ../../../os/ext/CMSIS/include/core_cmInstr.h \
+ ../../../os/ext/CMSIS/include/core_cmFunc.h \
+ ../../../os/ext/CMSIS/include/core_cmSimd.h \
+ ../../../os/ext/CMSIS/ST/STM32F3xx/system_stm32f3xx.h
+
+/usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdbool.h:
+
+/usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/include/stdint.h:
+
+/usr/local/bin/../lib/gcc/arm-none-eabi/4.7.2/../../../../arm-none-eabi/include/stdint.h:
+
+../../../os/common/ports/ARMCMx/compilers/GCC/vectors.h:
+
+../../../os/common/ports/ARMCMx/devices/STM32F3xx/cmparams.h:
+
+../../../os/hal/boards/ST_NUCLEO_F302R8/board.h:
+
+../../../os/ext/CMSIS/ST/STM32F3xx/stm32f3xx.h:
+
+../../../os/ext/CMSIS/ST/STM32F3xx/stm32f302x8.h:
+
+../../../os/ext/CMSIS/include/core_cm4.h:
+
+../../../os/ext/CMSIS/include/core_cmInstr.h:
+
+../../../os/ext/CMSIS/include/core_cmFunc.h:
+
+../../../os/ext/CMSIS/include/core_cmSimd.h:
+
+../../../os/ext/CMSIS/ST/STM32F3xx/system_stm32f3xx.h:
